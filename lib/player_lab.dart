6f975b881b8992e2e09c8982d8f17860d59365f4
achrefbs <siachrefboularess@gmail.com>
2022-10-11 13:42:44 +0100

    update

--- lib/player_lab.dart
+++ lib/player_lab.dart
@@ -1,7 +1,50 @@
 import 'package:fantasyapp/models/player.dart';
 
 class PlayerLab {
-  final List<Player> players = [];
+  final List<Player> players = [
+    Player(0, "firstName", "lastName", "GK", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(1, "firstName", "lastName", "GK", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(2, "firstName", "lastName", "GK", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(3, "firstName", "lastName", "DEF", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(4, "firstName", "lastName", "DEF", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(5, "firstName", "lastName", "DEF", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(6, "firstName", "lastName", "MID", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(7, "firstName", "lastName", "MID", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(8, "firstName", "lastName", "MID", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(9, "firstName", "lastName", "FWD", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0),
+    Player(10, "firstName", "lastName", "FWD", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(11, "firstName", "lastName", "FWD", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(12, "firstName", "lastName", "FWD", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(13, "firstName", "lastName", "FWD", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(14, "firstName", "lastName", "MID", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(15, "firstName", "lastName", "MID", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(16, "firstName", "lastName", "MID", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(17, "firstName", "lastName", "MID", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(18, "firstName", "lastName", "DEF", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(19, "firstName", "lastName", "DEF", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+    Player(20, "firstName", "lastName", "DEF", 1, 5.5, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0),
+  ];
   static PlayerLab sPlayerLab = PlayerLab();
 
   static PlayerLab get() {
